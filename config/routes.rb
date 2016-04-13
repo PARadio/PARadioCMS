b0b5fdc77f2b482ddd59a0abdd800782614b873f
dasEinhorn1 <achayward1@gmail.com>
2016-04-13 13:20:07 -0400

    styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :subjects, :pages, :episodes, :sections, :mediafiles, :streamitems
 
+  root 'home#index'
   # mp3 file uploading
 
   # The priority is based upon order of creation: first created -> highest priority.
