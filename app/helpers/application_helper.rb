c25e64683584566c6ee98b0947df49c38dbc1561
Sean Kelley <sgtkode01@gmail.com>
2016-04-18 13:19:32 -0400

    added global glyphicon helper; started to style livestream page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def error_messages_for(object)
     render(:partial=> 'shared/error_messages',:locals => {:object => object})
   end
+
+  def echoGlyphicon (name)
+    return "<span class='glyphicon glyphicon-#{name}' aria-hidden='true'></span>".html_safe
+  end
 end
