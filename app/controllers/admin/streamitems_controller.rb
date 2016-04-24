31608cb2e89f2dfa3e2f31ddcee50a3830be967d
sgtkode <sgtkode01@gmail.com>
2016-04-24 18:47:29 -0400

    fixed progress bar glitch when schedule was empty

--- app/controllers/admin/streamitems_controller.rb
+++ app/controllers/admin/streamitems_controller.rb
@@ -10,9 +10,13 @@ class Admin::StreamitemsController < ApplicationController
     @selected_date = Date.strptime(datestr, '%Y-%m-%d')
     @streamitems = Admin::Streamitem.where(date: @selected_date.strftime('%Y-%m-%d')).sorted
 
-    @time_taken = TimeDifference.between(Admin::Streamitem.stream_start, @streamitems.last.start_time + @streamitems.last.episode.duration.seconds).in_seconds
-    @total_time = TimeDifference.between(Admin::Streamitem.stream_start, Admin::Streamitem.stream_end).in_seconds
-    @percent_left = ((@time_taken / @total_time) * 100).round(2)
+    if @streamitems.empty?
+      time_taken = 0
+    else
+      time_taken = TimeDifference.between(Admin::Streamitem.stream_start, @streamitems.last.start_time + @streamitems.last.episode.duration.seconds).in_seconds
+    end
+    total_time = TimeDifference.between(Admin::Streamitem.stream_start, Admin::Streamitem.stream_end).in_seconds
+    @percent_left = ((time_taken / total_time) * 100).round(2)
 
     if @percent_left >= 90
       @progess_class = 'danger'
@@ -22,7 +26,11 @@ class Admin::StreamitemsController < ApplicationController
       @progess_class = 'success'
     end
 
-    time_available_hrs = TimeDifference.between(@streamitems.last.start_time + @streamitems.last.episode.duration.seconds, Admin::Streamitem.stream_end).in_hours
+    if @streamitems.empty?
+      time_available_hrs = TimeDifference.between(Admin::Streamitem.stream_start, Admin::Streamitem.stream_end).in_hours
+    else
+      time_available_hrs = TimeDifference.between(@streamitems.last.start_time + @streamitems.last.episode.duration.seconds, Admin::Streamitem.stream_end).in_hours
+    end
     time_available_min = ("0." + time_available_hrs.to_s.split('.').last).to_f * 60
     time_available_sec = ("0." + time_available_min.to_s.split('.').last).to_f * 60
     @time_available_str = time_available_hrs.to_i.to_s + "h " + time_available_min.to_i.to_s + "m " + time_available_sec.to_i.to_s + "s"
