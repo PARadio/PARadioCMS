88067e6e163f712ef71d76cafcd995ce821e7ac4
sgtkode <sgtkode01@gmail.com>
2016-05-01 20:01:27 -0400

    more capistrano tweaking

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,12 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :rbenv_type, :system
+set :rbenv_ruby, '2.3.1'
+set :rbenv_custom_path, "$HOME/.rbenv"
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_custom_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_custom_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
 
 ## Defaults:
 # set :scm,           :git
